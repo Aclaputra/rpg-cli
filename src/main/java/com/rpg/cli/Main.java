4c823e5dad0dacb1a7838672565345b175ceba94
Muhammad Acla <aclalead@gmail.com>
2024-02-11 15:58:21 +0700

    feat!: remove main interface

--- src/main/java/com/rpg/cli/Main.java
+++ src/main/java/com/rpg/cli/Main.java
@@ -1,16 +1,9 @@
 package com.rpg.cli;
 
-interface MainInterface {
-    String hello(String name);
-}
-public class Main implements MainInterface {
+
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
-
-    @Override
-    public String hello(String name) {
-        return "Hello " + name;
     }
 }
